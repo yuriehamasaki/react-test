ef993b90fc54aa1733f79af5e86b1d397a66316f
hamasaki <hamasaki@team-lab.com>
2016-04-11 18:30:29 +0900

    変数名変更、テスト終了

--- dist/app.js
+++ dist/app.js
@@ -2,215 +2,121 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
-// initialized the varriable
-// array
-// ����Ҏ�t�������ߤ��狼������
-// �����������Ԥ����Τ�State����
-var _tmpValue = []; //�����z���Ǥ������Ȥ�����ζ�ˤ��루_tmpValue��
-// initialized the varriable
-// this is number of sum. :]
-var sum = 0;
-var num = 0;
-// ��Ӌ��Ӌ�㤬�Ф��줿�����Єe
-// true: -> Ӌ�㤷���� false: -> Ӌ�㤷�Ƥʤ�
-var isSum = false;
-
-var KeySample = React.createClass({displayName: "KeySample",
-  getInitialState() {
-    return {
+var Calculator = React.createClass({displayName: "Calculator",
+  getInitialState(){
+    repository = [];
+    firstBox = 0;
+    secBox = 0;
+    totalCount = false;
+    return{
       buttons: [1, 2, 3, "+",4, 5, 6,"-", 7, 8, 9, "*", 0, "C",  "/", "="],
       text: [].join('')
     };
   },
-  onClick(value) {
-    //console.log(this.state);
-    //console.log('value is ' + value);
-    var _length = _tmpValue.length;
-
-    // save the data which pressed the button
-    if (value == '='){
-      // first-click
-      if(_length <= 0) {
+  onClick(value){
+    var strLength = repository.length;
+    if (value == '=') {
+      if (strLength <= 0){
         return;
       } else {
-        // �Τ�����tmpValue�����֤����äƤ�
-        // ex: 4/2�򤷤������� 4/���Ȥ���Ѻ���줿���ϺΤ⤷�ʤ��褦�ˤ��롣
-        if(_tmpValue[_length - 1] === '+' || _tmpValue[_length - 1] === '-' || _tmpValue[_length - 1] === '*' || _tmpValue[_length - 1] === '/') {
+        if (repository[strLength - 1] === '+' || repository[strLength - 1] === '-' || repository[strLength - 1] === '*' || repository[strLength - 1] === '/'){
           return;
         }
       }
-      //console.log('whats value ' + value);
-      this.calculate(); //Ӌ�㤷�Ƥ��Ȥ���������
-
-    } else if (value == 'C') { // reset the array, cause pressed equa;l or 'C'
-
-      ecalculate = []; //�դˤ���
-      _tmpValue = []; //��ʾ�����Ƥ벿�֤��դˤ���
-      isSum = false;// ��Ӌ���Ƥʤ����Ȥˤ���
-      sum = 0;
-
+      this.calculate();
+    } else if (value == 'C') {
+      calculate = [];
+      repository = [];
+      totalCount = false;
+      firstBox = 0;
       this.setState({
-        text: '' //ӛ�ŤΈ��Ϥ��Τޤ�text��׷��
+        text: ''
       });
-
     } else {
-      // ���֤� +, -, * or /�����äƤ��롣
-      // �ʤΤǡ��⤷�����Τ����äƤʤ��r�������Εr�������������ʤ��Τ⤷�ʤ��I������Ҫ:)
-      console.log('click', _tmpValue);
-
-      // first-click
-      if(_length <= 0) {
-        // ������+, -, * or /�������å����줿�Ȥ�
-        if(value === '+' || value == '-' || value === '*' || value == '/') {
-            
-          // �����ǤϤޤ���Ӌ���g�Ф����Ƥʤ��Τ�
-          if(!isSum) {
-            // �ʤˤ⤳�����Ϥ����ʤ��no-more...;) 
+      if (strLength <= 0) {
+        if (value === '+' || value === '-' || value === '*'|| value === '/') {
+          if (!totalCount) {
             return;
           } else {
-            // �����Ϻ�Ӌ��Ӌ�㤬�g�Ф����Ƥ��Τ�
-            // tampValue��sum����Ӌ�νY���������뤷�Ƥ���
-            _tmpValue = [sum];
+            repository = [firstBox];
           }
         }
-      } else if(_length >= 1) {
-        // �������Ǥ����äƤ��r�Ǥ��ġ�00��'0'���B�A���ʤ��褦��
-        if(value === 0) {
+      } else if (strLength >= 1){
+        if (value === 0) {
           return;
         }
-
-        // ���FѺ���줿�Τ�ӛ�ŤǤ���һ��ǰ��Ѻ���줿�Τ�ӛ�Ť����Єe
-        // ԓ���������ϤϺΤ⤷�ʤ�
-        
-        // ���F��Ѻ���줿����ӛ�Ťʤ�
-        if(value === '+' || value === '-' || value === '*' || value === '/') {
-          // һ��ǰ�΂���ӛ�Ťʤ�
-          if(_tmpValue[_length - 1] === '+' || _tmpValue[_length - 1] === '-' || _tmpValue[_length - 1] === '*' || _tmpValue[_length - 1] === '/') {
+        if (value === '+' || value === '-' || value === '*' || value === '/'){
+          if (repository[strLength - 1] === '+' || repository[strLength - 1] === '-' || repository[strLength - 1] === '*' || repository[strLength - 1] === '/'){
             return;
           }
         }
-        
-        isSum = false;// ��Ӌ���Ƥʤ����Ȥˤ���
-      } 
-
-      _tmpValue.push(value); //input���Ф˥����å����줿�����ͤ�
-
-      this.setState({
-        text: _tmpValue.join('') //ӛ�ŤΈ��Ϥ��Τޤ�text��׷��
-      });
-
-      //console.log('push ' + value);
-    }
-    //console.log(this.state.text);
-    //console.log(event.target);
-  },
-  // =Ѻ���줿�Ȥ���Ӌ�㤵����
-  calculate() { //Ӌ�㤹���Ȥ�����
-  
-    var flag;
-
-    var str = _tmpValue.join("");//_tmpValue�����л������������äƤ루str��
-    //���У��᥽�åȣ�����
-
-    var array = str.split(/(\+|\-|\*|\/)/); 
-
-    for (var v of array) {
-      //console.log('v is ' + v);
-
-      if(v == '+'){
-        flag = v;
-        //console.log('+ ' + flag);
-      } else if(v == '-'){
-        flag = v;
-        //console.log('- ' + flag);
-      } else if(v == '*'){
-        flag = v;
-        //console.log('* ' + flag);
-      }  else if(v == '/'){
-        flag = v;
-        //console.log('/ ' + flag);
-      } else if(v == '='){
-        flag = v;
-        //console.log('+ ' + flag);
-      } else { 
-        // this could be number :) yeah!!! 
-        // wanna Magic Number!!!! :)
-        // console.log('Magic Number is ' + v);
-
-        num = array[0] * 1;
-        sum = array[2] * 1;
-
-        // num = parseInt(array[0]);
-        // sum = parseInt(array[2]);
-
-        // console.log('whats else num ' + num);
-        // console.log('whats else sum ' + sum);
-
+        totalCount = false;
       }
-    }// end of for!!!!
-
-    //console.log('whats else num ' + num);
-    //console.log('whats else sum ' + sum);
-
-    // let's sum
-    // 3+6=9 -> this is wrong!!!!!!!!
-    // this displayed just 9
-
-    // sum =  �������줿�� + �������줿�� 
-    // currently, sum is 0, also num is 0. OMG
-
-    //var aclear;
-
-    if(flag == '+') {
-      sum = num + sum;
-    } else if(flag == '-') {
-      sum = num - sum;
-    } else if(flag == '*') {
-      sum = num * sum;
-    } else if(flag == '/') {
-      sum = num / sum;
+      repository.push(value);
+      this.setState({
+        text: repository.join('')
+      });
     }
-    // Ӌ�㤬�g�Ф��줿�Τ�true�ˡ�
-    isSum = true;
-
-    /*else {
-      aclear(_tmpValue);// <- this is ERROR!!!!!!!!
-      console.log('whats else ' + sum);
-    }*/
-
-    console.log('sum', sum);
-
-    // if sum is 8
-    // _tmpValue = [8]
-    _tmpValue = []; //���֤�Ѻ���줿���դˤ���
-    
-    this.setState({
-      text: sum // Ӌ���Y������ʾ��
-    });
-
-
+    console.log('koko' + this.state.text);
   },
-  render() {
-    var _this = this;
+  calculate() {
+    var symbol;
+    var repoStr = repository.join("");
+    var array = repoStr.split(/(\+|\-|\*|\/)/);
 
-    var buttonElements = this.state.buttons.map(function(button, i) {
-      return (
-        React.createElement("button", {key: i, onClick: function() {
-          _this.onClick(button);
-        }}, button)
-      );
+    for (var character of array) {
+      if(character == '+'){
+        symbol = character;
+      } else if (character == '-'){
+        symbol = character;
+      } else if (character == '*'){
+        symbol = character;
+      } else if (character == '/'){
+        symbol = character;
+      } else if (character == '='){
+        symbol = character;
+      } else{
+        firstBox  = parseInt(array[0]);
+        secBox    = parseInt(array[2]);
+      }
+    }
+
+    if(symbol == '+') {
+      secBox = firstBox + secBox;
+    } else if (symbol == '-') {
+      secBox = firstBox - secBox;
+    } else if (symbol == '*') {
+      secBox = firstBox * secBox;
+    } else if (symbol == '/') {
+      secBox = firstBox / secBox;
+    }
+
+    totalCount = true;
+
+    repository = [];
+    this.setState({
+      text: secBox
     });
-    return (
-      React.createElement("div", null, 
-        React.createElement("input", {type: "text", name: "", value: this.state.text, readOnly: true}), 
-        React.createElement("div", {id: "keyboard"}, buttonElements)
-      )
-    );
+  },
+  render(){
+    var self = this;
+      var buttonElements = this.state.buttons.map(function(button, i) {
+          return (
+            React.createElement("button", {key: i, onClick: function() {
+                self.onClick(button);
+            }}, button)
+          );
+      });
+      return (
+          React.createElement("div", null, 
+            React.createElement("input", {type: "text", name: "", value: this.state.text, readOnly: true}), 
+            React.createElement("div", {id: "keyboard"}, buttonElements)
+          )
+      );
   }
 });
 
-
-ReactDom.render(React.createElement(KeySample, null), document.getElementById("calculator_wrap"));
+ReactDom.render(React.createElement(Calculator, null), document.getElementById("calculator_wrap"));
 
 },{"react":159,"react-dom":30}],2:[function(require,module,exports){
 (function (process){
--- js/main.js
+++ js/main.js
@@ -1,218 +1,118 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
-// initialized the varriable
-// array
-// 命名規則を第三者がわかるもの
-// 変数名を宣言するのはStateの中
-var _tmpValue = []; //溜め込んでる場所という意味にする（_tmpValue）
-// initialized the varriable
-// this is number of sum. :]
-var sum = 0;
-var num = 0;
-// 合計の計算が行われたかの判別
-// true: -> 計算した。 false: -> 計算してない
-var isSum = false;
-
-var KeySample = React.createClass({
-  getInitialState() {
-    return {
+var Calculator = React.createClass({
+  getInitialState(){
+    repository = [];
+    firstBox = 0;
+    secBox = 0;
+    totalCount = false;
+    return{
       buttons: [1, 2, 3, "+",4, 5, 6,"-", 7, 8, 9, "*", 0, "C",  "/", "="],
       text: [].join('')
     };
   },
-  onClick(value) {
-    //console.log(this.state);
-    //console.log('value is ' + value);
-    var _length = _tmpValue.length;
-
-    // save the data which pressed the button
-    if (value == '='){
-      // first-click
-      if(_length <= 0) {
+  onClick(value){
+    var strLength = repository.length;
+    if (value == '=') {
+      if (strLength <= 0){
         return;
       } else {
-        // 何かしらtmpValueに数字が入ってる
-        // ex: 4/2をしたいけど 4/＝とかを押された場合何もしないようにする。
-        if(_tmpValue[_length - 1] === '+' || _tmpValue[_length - 1] === '-' || _tmpValue[_length - 1] === '*' || _tmpValue[_length - 1] === '/') {
+        if (repository[strLength - 1] === '+' || repository[strLength - 1] === '-' || repository[strLength - 1] === '*' || repository[strLength - 1] === '/'){
           return;
         }
       }
-      //console.log('whats value ' + value);
-      this.calculate(); //計算してるところを呼ぶ
-
-    } else if (value == 'C') { // reset the array, cause pressed equa;l or 'C'
-
-      ecalculate = []; //空にする
-      _tmpValue = []; //表示されてる部分も空にする
-      isSum = false;// 合計してないことにする
-      sum = 0;
-
+      this.calculate();
+    } else if (value == 'C') {
+      calculate = [];
+      repository = [];
+      totalCount = false;
+      firstBox = 0;
       this.setState({
-        text: '' //記号の場合そのままtextに追加
+        text: ''
       });
-
     } else {
-      // 数字か +, -, * or /が入ってくる。
-      // なので、もし値が何も入ってない時（最初の時）に数字以外なら何もしない処理が必要:)
-      console.log('click', _tmpValue);
-
-      // first-click
-      if(_length <= 0) {
-        // ここは+, -, * or /がクリックされたとき
-        if(value === '+' || value == '-' || value === '*' || value == '/') {
-            
-          // ここではまだ合計が実行されてないので
-          if(!isSum) {
-            // なにもこれ以上させないわ。no-more...;) 
+      if (strLength <= 0) {
+        if (value === '+' || value === '-' || value === '*'|| value === '/') {
+          if (!totalCount) {
             return;
           } else {
-            // ここは合計の計算が実行されてるので
-            // tampValueにsum（合計の結果）を代入しておく
-            _tmpValue = [sum];
+            repository = [firstBox];
           }
         }
-      } else if(_length >= 1) {
-        // 値がすでに入ってる時でかつ、00と'0'が連続こないように
-        if(value === 0) {
+      } else if (strLength >= 1){
+        if (value === 0) {
           return;
         }
-
-        // 今現押されたのが記号でかつ一個前に押されたのが記号かの判別
-        // 該当する場合は何もしない
-        
-        // 今現在押された値が記号なら
-        if(value === '+' || value === '-' || value === '*' || value === '/') {
-          // 一個前の値が記号なら
-          if(_tmpValue[_length - 1] === '+' || _tmpValue[_length - 1] === '-' || _tmpValue[_length - 1] === '*' || _tmpValue[_length - 1] === '/') {
+        if (value === '+' || value === '-' || value === '*' || value === '/'){
+          if (repository[strLength - 1] === '+' || repository[strLength - 1] === '-' || repository[strLength - 1] === '*' || repository[strLength - 1] === '/'){
             return;
           }
         }
-        
-        isSum = false;// 合計してないことにする
-      } 
-
-      _tmpValue.push(value); //inputの中にクリックされた値を送る
-
-      this.setState({
-        text: _tmpValue.join('') //記号の場合そのままtextに追加
-      });
-
-      //console.log('push ' + value);
-    }
-    //console.log(this.state.text);
-    //console.log(event.target);
-  },
-  // =押されたときに計算させる
-  calculate() { //計算するところ↓
-  
-    var flag;
-
-    var str = _tmpValue.join("");//_tmpValue文字列化した値が入ってる（str）
-    //配列＋メソット＋引数
-
-    var array = str.split(/(\+|\-|\*|\/)/); 
-
-    for (var v of array) {
-      //console.log('v is ' + v);
-
-      if(v == '+'){
-        flag = v;
-        //console.log('+ ' + flag);
-      } else if(v == '-'){
-        flag = v;
-        //console.log('- ' + flag);
-      } else if(v == '*'){
-        flag = v;
-        //console.log('* ' + flag);
-      }  else if(v == '/'){
-        flag = v;
-        //console.log('/ ' + flag);
-      } else if(v == '='){
-        flag = v;
-        //console.log('+ ' + flag);
-      } else { 
-        // this could be number :) yeah!!! 
-        // wanna Magic Number!!!! :)
-        // console.log('Magic Number is ' + v);
-
-        num = array[0] * 1;
-        sum = array[2] * 1;
-
-        // num = parseInt(array[0]);
-        // sum = parseInt(array[2]);
-
-        // console.log('whats else num ' + num);
-        // console.log('whats else sum ' + sum);
-
+        totalCount = false;
       }
-    }// end of for!!!!
-
-    //console.log('whats else num ' + num);
-    //console.log('whats else sum ' + sum);
-
-    // let's sum
-    // 3+6=9 -> this is wrong!!!!!!!!
-    // this displayed just 9
-
-    // sum =  入力された値 + 入力された値 
-    // currently, sum is 0, also num is 0. OMG
-
-    //var aclear;
-
-    if(flag == '+') {
-      sum = num + sum;
-    } else if(flag == '-') {
-      sum = num - sum;
-    } else if(flag == '*') {
-      sum = num * sum;
-    } else if(flag == '/') {
-      sum = num / sum;
+      repository.push(value);
+      this.setState({
+        text: repository.join('')
+      });
     }
-    // 計算が実行されたのでtrueに。
-    isSum = true;
-
-    /*else {
-      aclear(_tmpValue);// <- this is ERROR!!!!!!!!
-      console.log('whats else ' + sum);
-    }*/
-
-    console.log('sum', sum);
-
-    // if sum is 8
-    // _tmpValue = [8]
-    _tmpValue = []; //数字を押されたら空にする
-    
-    this.setState({
-      text: sum // 計算結果を表示　
-    });
-
-
+    console.log('koko' + this.state.text);
   },
-  render() {
-    var _this = this;
+  calculate() {
+    var symbol;
+    var repoStr = repository.join("");
+    var array = repoStr.split(/(\+|\-|\*|\/)/);
 
-    var buttonElements = this.state.buttons.map(function(button, i) {
-      return (
-        <button key={i} onClick={function() {
-          _this.onClick(button);
-        }}>{button}</button>
-      );
+    for (var character of array) {
+      if(character == '+'){
+        symbol = character;
+      } else if (character == '-'){
+        symbol = character;
+      } else if (character == '*'){
+        symbol = character;
+      } else if (character == '/'){
+        symbol = character;
+      } else if (character == '='){
+        symbol = character;
+      } else{
+        firstBox  = parseInt(array[0]);
+        secBox    = parseInt(array[2]);
+      }
+    }
+
+    if(symbol == '+') {
+      secBox = firstBox + secBox;
+    } else if (symbol == '-') {
+      secBox = firstBox - secBox;
+    } else if (symbol == '*') {
+      secBox = firstBox * secBox;
+    } else if (symbol == '/') {
+      secBox = firstBox / secBox;
+    }
+
+    totalCount = true;
+
+    repository = [];
+    this.setState({
+      text: secBox
     });
-    return (
-      <div>
-        <input type="text" name="" value={this.state.text} readOnly></input>
-        <div id="keyboard">{buttonElements}</div>
-      </div>
-    );
+  },
+  render(){
+    var self = this;
+      var buttonElements = this.state.buttons.map(function(button, i) {
+          return (
+            <button key={i} onClick={function() {
+                self.onClick(button);
+            }}>{button}</button>
+          );
+      });
+      return (
+          <div>
+            <input type="text" name="" value={this.state.text} readOnly></input>
+            <div id="keyboard">{buttonElements}</div>
+          </div>
+      );
   }
 });
 
-
-ReactDom.render(<KeySample />, document.getElementById("calculator_wrap"));
-
-
-
-
-
-
+ReactDom.render(<Calculator />, document.getElementById("calculator_wrap"));
